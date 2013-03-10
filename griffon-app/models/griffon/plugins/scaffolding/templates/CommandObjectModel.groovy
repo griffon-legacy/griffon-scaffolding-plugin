656efd281e810987c42a080e3659315355d012ab
aalmiray <aalmiray@gmail.com>
2013-03-10 22:14:24 +0100

    Templates should go into toolkit specific plugins

--- griffon-app/models/griffon/plugins/scaffolding/templates/CommandObjectModel.groovy
+++
@@ -1,9 +0,0 @@
-package griffon.plugins.scaffolding.templates
-
-class CommandObjectModel {
-    @Bindable String title
-    @Bindable int width = 0
-    @Bindable int height = 0
-    @Bindable boolean resizable = true
-    @Bindable boolean modal = true
-}
